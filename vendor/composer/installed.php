42b268d7cae6c52705d95efc366879bea2026c89
Abdellah Ramadan <ramadanabdel24@gmail.com>
2021-11-04 14:16:53 +0100

    Make all the functions static, add various functions to manipulate string

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'bee26e93917cbfd4b13191136246d32c9ae5c13e',
+    'name' => 'abdellahramadan/string',
+  ),
+  'versions' => 
+  array (
+    'abdellahramadan/string' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'bee26e93917cbfd4b13191136246d32c9ae5c13e',
+    ),
+  ),
+);
